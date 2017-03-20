7947a26f4284bfe3e16131b981ecf723c71a1443
Samuli Suomi <sasuom@gmail.com>
2017-03-20 10:09:50 +0200

    Add flexbox-grid and handle tap of dummy button

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import { Grid, Row, Col } from 'react-flexbox-grid';
+import AppBar from 'material-ui/AppBar';
 import DummyComponent from './DummyComponent';
 import './App.css';
 
 class App extends Component {
+  sayHello() {
+    console.log("hello");
+  }
+
   render() {
     return (
       <MuiThemeProvider>
-        <DummyComponent />
+        <Grid fluid className="App-Grid">
+          <Row className="App-Bar-Row">
+            <AppBar className="App-Bar"
+              title="Awesome Empty React Project"
+            />
+          </Row>
+          <div className="App-Container">
+            <Row className="Row middle-xs">
+              <Col className="end-md" xs={12} sm={12} md={6} lg={6}>
+                <p>This is some text.</p>
+              </Col>
+              <Col className="start-md" xs={12} sm={12} md={6} lg={6}>
+                <DummyComponent sayHello={ this.sayHello } />
+              </Col>
+            </Row>
+          </div>
+        </Grid>
       </MuiThemeProvider>
     );
   }
--- src/DummyComponent.js
+++ src/DummyComponent.js
@@ -2,8 +2,14 @@ import React from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
 import './DummyComponent.css';
 
-const DummyComponent = () => (
-  <RaisedButton label="Default" />
-);
+class DummyComponent extends React.PureComponent {
+  render() {
+    return <div className="Dummy-Component">
+      <RaisedButton
+        label="Hello"
+        onTouchTap={ this.props.sayHello } />
+    </div>
+  }
+}
 
 export default DummyComponent;
